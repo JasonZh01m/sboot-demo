fcaa35d95e508eaede871a8597a134a8c8d6084a
zhouxh <xinghuai_zhou@163.com>
2017-08-18 15:49:05 +0800

    整合Redis

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,33 +2,23 @@ package com.example.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.io.Serializable;
+
 /**
  * Created by JasonZh on 2017/8/16.
  */
-public class User {
+public class User implements Serializable {
 
-    @Id
-    private Long id;
-
+    private static final long serialVersionUID = -1L;
     private String username;
-
     private Integer age;
 
-    public User(Long id, String username, Integer age) {
-        this.id = id;
+    public User(String username, Integer age) {
         this.username = username;
         this.age = age;
     }
     // 省略getter和setter
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUsername() {
         return username;
@@ -45,4 +35,5 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+
 }
