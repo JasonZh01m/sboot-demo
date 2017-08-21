f93b9399fd3fbfc4a3e1f107c3112062dcaea61c
zhouxh <xinghuai_zhou@163.com>
2017-08-21 18:02:00 +0800

    MyBatis注解以及事务处理@Rollback

--- src/main/java/com/example/mapper/UserMapper.java
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -1,21 +1,39 @@
 package com.example.mapper;
 
 import com.example.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * Created by JasonZh on 2017/8/21.
  */
 public interface UserMapper {
 
-    @Select("SELECT * FROM USER WHERE NAME = #{name} limit 1")
+    @Select("SELECT * FROM USER WHERE NAME = #{name} order by id desc limit 1")
     User findByName(@Param("name") String name);
 
     @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
     int insert(@Param("name") String name, @Param("age") Integer age);
+
+    @Update("UPDATE user SET age=#{age} WHERE name=#{name}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
+    /**
+     * 而对于“查”操作，我们往往需要进行多表关联，汇总计算等操作，那么对于查询的结果
+     * 往往就不再是简单的实体对象了，往往需要返回一个与数据库实体不同的包装类，
+     * 那么对于这类情况，就可以通过<em>@Results</em>和<em>@Result</em>注解来进行绑定
+     * @return
+     */
+    @Results({
+            @Result(property = "name", column = "name"),
+            @Result(property = "age", column = "age")
+    })
+    @Select("SELECT name, age FROM user")
+    List<User> findAll();
 
 
 }
--- src/test/java/com/example/embedded/MybatisTest.java
+++ src/test/java/com/example/embedded/MybatisTest.java
@@ -13,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by JasonZh on 2017/8/18.
@@ -20,6 +23,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Transactional
 public class MybatisTest {
 
     private final static Logger logger = LoggerFactory.getLogger(MybatisTest.class);
@@ -36,6 +40,39 @@ public class MybatisTest {
         logger.info("{}", u);
 
         Assert.assertEquals(20, u.getAge().intValue());
+    }
+
+
+    @Test
+    @Rollback
+    public void testUserMapper() throws Exception {
+        // insert一条数据，并select出来验证
+        userMapper.insert("AAA", 20);
+        User u = userMapper.findByName("AAA");
+        logger.info("{}", u);
+        Assert.assertEquals(20, u.getAge().intValue());
+        // update一条数据，并select出来验证
+        u.setAge(30);
+        userMapper.update(u);
+        u = userMapper.findByName("AAA");
+        logger.info("{}", u);
+        Assert.assertEquals(30, u.getAge().intValue());
+        // 删除这条数据，并select验证
+        userMapper.delete(u.getId());
+        u = userMapper.findByName("AAA");
+        logger.info("{}", u);
+        Assert.assertEquals(null, u);
+    }
+
+
+    @Test
+    @Rollback
+    public void testUserMapper2() {
+        List<User> userList = userMapper.findAll();
+        for(User user : userList) {
+            Assert.assertEquals(null, user.getId());
+            Assert.assertNotEquals(null, user.getName());
+        }
     }
 
 }
